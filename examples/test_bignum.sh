badabd6da6946f4115ecfcb29e0b42e1dc9999b2
Rod <lancelot.b@outlook.com>
2016-03-06 10:44:06 +0800

    060316#03: update test_bignum.sh

--- examples/test_bignum.sh
+++ examples/test_bignum.sh
@@ -2,8 +2,11 @@
 
 BASEDIR=$(dirname $0)
 
+echo "ink:"
 time ink $BASEDIR/bignum.ink > out1
+echo -e "\npython:"
 time python $BASEDIR/bignum.py > out2
+echo -e "\nruby:"
 time ruby $BASEDIR/bignum.rb > out3
 
 echo ""
