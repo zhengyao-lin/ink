e6ce4318b378330102a419885421367a04e06e82
Rod <lancelot.b@outlook.com>
2016-04-17 16:10:34 +0800

    170416#01: modify inttype.h

--- core/inttype.h
+++ core/inttype.h
@@ -7,13 +7,13 @@ namespace ink {
 
 typedef int8_t			Ink_SInt8;
 typedef int16_t			Ink_SInt16;
-typedef int32_t 		Ink_SInt32;
-typedef int64_t		 	Ink_SInt64;
+typedef int32_t			Ink_SInt32;
+typedef int64_t			Ink_SInt64;
 
 typedef uint8_t			Ink_UInt8;
 typedef uint16_t		Ink_UInt16;
-typedef uint32_t 		Ink_UInt32;
-typedef uint64_t	 	Ink_UInt64;
+typedef uint32_t		Ink_UInt32;
+typedef uint64_t		Ink_UInt64;
 
 typedef Ink_UInt64		Ink_SizeType;
 typedef Ink_UInt8		Ink_Byte;
